591b4f5db9bb87e17621350c0977fb8f52eba663
ViniNovack <vininomn@gmail.com>
2026-05-06 21:22:44 -0300

    Atualização: Anotações aula 10

---
+++ aula_9/Aula_2_TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package aula_9;
+
+public class Aula_2_TiposPrimitivos {
+    public static void main(String[] args) {        // psvm     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        int idade = 10;                             // Exemplo de variavel
+        System.out.println("Ele tem " + idade + " anos.\n");
+    }
+}
+
+/* 
+    TIPOS PRIMITIVOS:
+    int, double, float, char, byte, short, long, boolean
+*/
