06e51d3048f4e677eb24c2f5f5736b480951ff36
ViniNovack <vininomn@gmail.com>
2026-05-07 21:36:22 -0300

    Update Aula_2_TiposPrimitivos.java

--- aula_9/Aula_2_TiposPrimitivos.java
+++ aula_9/Aula_2_TiposPrimitivos.java
@@ -3,10 +3,17 @@ package aula_9;
 public class Aula_2_TiposPrimitivos {
     public static void main(String[] args) {        // psvm     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         int idade = 10;                             // Exemplo de variavel
+        long numeroGrande = 100000;
         double salarioDouble = 2000;
         float salarioFloat = 2500;
-        
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 10;
+
         System.out.println("Ele tem " + idade + " anos.\n");
+        System.out.println(verdadeiro);
     }
 }
 
