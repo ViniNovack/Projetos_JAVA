5df79dc6d57d2a8b69b38272d3826f1b8447039e
ViniNovack <vininomn@gmail.com>
2026-05-15 16:48:44 -0300

    Atualização: Exercicio_2

--- aulas_15_a_19/Exercicios/Exercicio_2.java
+++ aulas_15_a_19/Exercicios/Exercicio_2.java
@@ -1,3 +1,12 @@
 public class Exercicio_2 {
-    
+    public static void main(String[] args){
+        double saldo = 1000.00;
+        saldo +=300;
+        saldo -=950;
+        if (saldo <= 0){
+            System.out.println("Alerta: Sua conta está no cheque especial! Saldo: " + "[" + saldo + "]");
+        } else{
+            System.out.println("Conta positiva! Saldo: " + "[" + saldo + "]");
+        }
+    }
 }
