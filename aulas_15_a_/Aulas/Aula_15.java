ac52c9f71a65b256d1b23789f821d4789d5ac689
ViniNovack <vininomn@gmail.com>
2026-05-15 15:39:16 -0300

    Atualização: Anotações de aula

--- aulas_15_a_/Aulas/Aula_15.java
+++ aulas_15_a_/Aulas/Aula_15.java
@@ -13,8 +13,8 @@ public class Aula_15 {
         System.out.println(resultado);
         //System.out.println(resultado02);
 
-        System.out.println();
 
+        System.out.println();
         //ATRIBUTOS
         boolean isDezMaiorQueVinte = 10 > 20;
         boolean isDezMenorQueVinte = 10 < 20;
@@ -28,9 +28,10 @@ public class Aula_15 {
         System.out.println(isDezIgualDez);
         System.out.println(isDezDiferenteDez);
 
-        System.out.println();
 
+        System.out.println();
         //OPERADORES LOGICOS
+        // &&
         int idade = 29;
         float salario = 3500F;
         boolean isDentroDaLei = idade >= 30 && salario >= 4612;
@@ -39,7 +40,44 @@ public class Aula_15 {
         System.out.println(isDentroDaLei);
         System.out.println(isDentroDaLeiMenorTrinta);
 
-    }   
+
+        System.out.println();
+        // ||
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel: " + isPlaystationCincoCompravel);
+
+
+        System.out.println();
+        // =, +=, -=, *=, /=, %=
+        double bonus = 1800;
+        bonus +=1000;
+        bonus -=300;
+        bonus *=2;
+        bonus /=2;
+        System.out.println(bonus);
+
+
+        System.out.println();
+        //OPERADORES UNARIOS
+        int contador = 0;
+        contador +=1;
+        contador++;    // contador +=1  ==  contador++
+        contador--;    // ...
+        ++contador;
+        --contador;
+        System.out.println(contador);
+
+        // Se tem uma diferença de colocar ++/-- na frente ou atraz, pois ele primeiro executa o que vem antes. Por exemplo:
+        int contador2;
+        contador2 = 2;
+        System.out.println("contador2++: " + contador2++);       //Nesse cado depois de imprimir na tela a variavel vai ser o 1 somado, porém depois pelo faro do ++ vir depois
+        contador2 = 2;
+        System.out.println("++contador2: " + ++contador2);
+    }
 }
 
 /*
