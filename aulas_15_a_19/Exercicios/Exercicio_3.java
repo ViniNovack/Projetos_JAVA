178cf9b11414200a20e1d41de6a45ebae816e9a5
ViniNovack <vininomn@gmail.com>
2026-05-15 17:08:31 -0300

    Atualização: Exercicio_3

--- aulas_15_a_19/Exercicios/Exercicio_3.java
+++ aulas_15_a_19/Exercicios/Exercicio_3.java
@@ -1,3 +1,15 @@
 public class Exercicio_3 {
-    ;
+    public static void main(String[] args){
+        double salario = 4500.50;
+        double imposto = 0;
+        if (salario <= 2000){
+            imposto = 0;
+        } else if (2000 < salario && salario <= 4000){                         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!IMPORTANTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            imposto = salario * 0.15;
+        } else if (salario > 4000){
+            imposto = salario * 0.2;
+        }
+
+        System.out.println("O salário é R$" + "[" + (int)salario + "]" + " e o imposto a ser pago ao Rei é de " + "[" + (int)imposto + "]" + " moedas de ouro.");
+    }
 }
